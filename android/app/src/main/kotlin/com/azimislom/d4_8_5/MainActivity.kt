13de8a9e5a47d3a9acb481398a34005c01b82e91
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-14 18:23:51 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/azimislom/d4_8_5/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.azimislom.d4_8_5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              // Square Image
+
+              // SingIn & Description
+
+              // Facebook & Google
+
+              // Email
+
+              // Password
+
+              // Forget Password
+
+              // Login
+
+              // go Sign Up
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
